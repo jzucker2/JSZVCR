376a0332a78b317db4978444f816464e77c9acaf
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-29 15:54:14 -0700

    Added tests for network response playback handling
    Strengthened confidence around network response loading

--- Example/Tests/JSZPlayerTestCase.m
+++ Example/Tests/JSZPlayerTestCase.m
@@ -20,6 +20,17 @@
     return NO;
 }
 
+- (void)setUp {
+    [super setUp];
+    XCTAssertNotNil(self.recordings);
+    XCTAssertNotEqual(self.recordings.count, 0);
+}
+
+- (void)tearDown {
+    XCTAssertNotEqual(self.recordings.count, 0);
+    [super tearDown];
+}
+
 - (void)testRecordedNetworkCall {
     [self performSimpleVerifiedNetworkCall:nil];
 }
