abee4db6dc7fce6f5e51d327beb5f2caa70117db
Jordan Zucker <jordan.zucker@gmail.com>
2015-06-29 13:49:27 -0700

    Updated tests to confirm unordered query matching works

--- Example/Tests/JSZUnorderedMatcherTestCase.m
+++ Example/Tests/JSZUnorderedMatcherTestCase.m
@@ -13,6 +13,8 @@
 
 #import "XCTestCase+XCTestCase_JSZVCRAdditions.h"
 
+typedef void (^UnorderedAssertions)(NSString *headerDate, NSDictionary *args);
+
 @interface JSZUnorderedMatcherTestCase : JSZVCRTestCase
 
 @end
@@ -38,30 +40,73 @@
     // ensure there is only one recording
     XCTAssertNotNil(self.recordings);
     XCTAssertEqual(self.recordings.count, 1);
-    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo=foo&bar=bar"];
-    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar=bar&foo=foo"];
+    UnorderedAssertions assertions = ^void(NSString *headerDate, NSDictionary *args) {
+        XCTAssertEqualObjects(headerDate, @"Mon, 29 Jun 2015 20:44:35 GMT");
+        NSDictionary *expectedArgsDict = @{
+                                           @"bar" : @"bar",
+                                           @"foo" : @"foo"
+                                           };
+        XCTAssertEqualObjects(args, expectedArgsDict);
+    };
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo=foo&bar=bar" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar=bar&foo=foo" withAssertions:assertions];
+}
+
+- (void)testUnorderedQueryNetworkCallWithThreeQueryParameters {
+    // ensure there is only one recording
+    XCTAssertNotNil(self.recordings);
+    XCTAssertEqual(self.recordings.count, 1);
+    UnorderedAssertions assertions = ^void(NSString *headerDate, NSDictionary *args) {
+        XCTAssertEqualObjects(headerDate, @"Mon, 29 Jun 2015 20:44:36 GMT");
+        NSDictionary *expectedArgsDict = @{
+                                           @"bar" : @"bar",
+                                           @"foo" : @"foo",
+                                           @"baz" : @"baz"
+                                           };
+        XCTAssertEqualObjects(args, expectedArgsDict);
+    };
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo=foo&bar=bar&baz=baz" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo=foo&baz=baz&bar=bar" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar=bar&baz=baz&foo=foo" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar=bar&foo=foo&baz=baz" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?baz=baz&bar=bar&foo=foo" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?baz=baz&foo=foo&bar=bar" withAssertions:assertions];
 }
 
 - (void)testAlternateUnorderedQueryNetworkCallWithNoQueryParameters {
     // ensure there is only one recording
     XCTAssertNotNil(self.recordings);
     XCTAssertEqual(self.recordings.count, 1);
-    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo&bar"];
-    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar&foo"];
+    UnorderedAssertions assertions = ^void(NSString *headerDate, NSDictionary *args) {
+        XCTAssertEqualObjects(headerDate, @"Mon, 29 Jun 2015 20:44:34 GMT");
+        NSDictionary *expectedArgsDict = @{
+                             @"bar" : @"",
+                             @"foo" : @""
+                             };
+        XCTAssertEqualObjects(args, expectedArgsDict);
+    };
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?foo&bar" withAssertions:assertions];
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get?bar&foo" withAssertions:assertions];
 }
 
 - (void)testSimpleNetworkCallWithNoQuery {
     // ensure there is only one recording
     XCTAssertNotNil(self.recordings);
     XCTAssertEqual(self.recordings.count, 1);
-    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get"];
+    UnorderedAssertions assertions = ^void(NSString *headerDate, NSDictionary *args) {
+        XCTAssertEqualObjects(headerDate, @"Mon, 29 Jun 2015 20:44:35 GMT");
+        NSDictionary *expectedArgsDict = @{
+                                           };
+        XCTAssertEqualObjects(args, expectedArgsDict);
+    };
+    [self verifiedSimpleNetworkCallWithURLString:@"https://httpbin.org/get" withAssertions:assertions];
 }
 
 - (void)testUniqueNetworkCallToProveNotAlwaysMatching {
     [self performUniqueVerifiedNetworkCall:nil];
 }
 
-- (void)verifiedSimpleNetworkCallWithURLString:(NSString *)URLString {
+- (void)verifiedSimpleNetworkCallWithURLString:(NSString *)URLString withAssertions:(UnorderedAssertions)assertions {
     NSParameterAssert(URLString);
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:URLString]];
     [self performNetworkRequest:request withVerification:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -72,28 +117,9 @@
         XCTAssertNil(error);
         NSLog(@"dataDict: %@", dataDict);
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        NSDictionary *expectedArgsDict;
-        NSString *headerFieldsDate;
-        if (self.invocation.selector == @selector(testSimpleUnorderedQueryNetworkCallWithQueryParameters)) {
-            headerFieldsDate = @"Fri, 26 Jun 2015 20:36:55 GMT";
-            expectedArgsDict = @{
-                                 @"bar" : @"bar",
-                                 @"foo" : @"foo"
-                                 };
-        } else if (self.invocation.selector == @selector(testAlternateUnorderedQueryNetworkCallWithNoQueryParameters)) {
-            headerFieldsDate = @"Fri, 26 Jun 2015 20:36:53 GMT";
-            expectedArgsDict = @{
-                                 @"bar" : @"",
-                                 @"foo" : @""
-                                 };
-        } else if (self.invocation.selector == @selector(testSimpleNetworkCallWithNoQuery)) {
-            headerFieldsDate = @"Fri, 26 Jun 2015 20:36:54 GMT";
-            expectedArgsDict = @{
-                                 };
+        if (assertions) {
+            assertions(httpResponse.allHeaderFields[@"Date"], dataDict[@"args"]);
         }
-        NSLog(@"%@", httpResponse.allHeaderFields[@"Date"]);
-        XCTAssertEqualObjects(httpResponse.allHeaderFields[@"Date"], headerFieldsDate);
-        XCTAssertEqualObjects(dataDict[@"args"], expectedArgsDict);
     }];
 }
 
